f3d1d5c71426ab47925b2ee9177bcc18cdb57a7f
sharifabee <sharifabee.alur@gmail.com>
2025-02-01 15:50:43 +0530

    add 5lacs stu records

--- StudentDB.java
+++ StudentDB.java
@@ -1,7 +1,10 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
 import java.util.ArrayList;
 
 public class StudentDB {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 	
 		ArrayList<Student> stuList = 
 				new ArrayList<Student>();
@@ -9,8 +12,8 @@ public class StudentDB {
 		for(int i=1;i<=500000;i++) {
 			
 			stu = new Student(
-					GenerateRandom.getNo(13000, 28000)
-					,"Stu"+i,"StPeters",
+					i,
+					"Stu"+i,"StPeters",
 					GenerateRandom.getNo(3000, 65000),
 					GenerateRandom.getNo(13,100),
 					GenerateRandom.getNo(23,89),
@@ -21,10 +24,26 @@ public class StudentDB {
 		
 		}
 		
-		System.out.println(stuList);
 		
 		
 		
+		PrintStream o = new PrintStream(new File("A.txt"));
+		 
+        // Store current System.out
+        // before assigning a new value
+        PrintStream console = System.out;
+ 
+        // Assign o to output stream
+        // using setOut() method
+        System.setOut(o);
+ 
+        // Display message only
+        System.out.println(stuList);
+		
+        // Use stored value for output stream
+        System.setOut(console);
+ 
+        
 		
 		
 		
